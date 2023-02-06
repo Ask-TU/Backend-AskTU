822290d6823c0a57f474ca684841e90c9697e29f
GuyMonmanut <78584452+GuyMonmanut@users.noreply.github.com>
2023-02-06 23:11:17 +0700

    Create classModel.go
    Class Model

---
+++ models/classModel.go
@@ -0,0 +1,16 @@
+package models
+
+type Class struct {
+	Subject_name string     `json:"subject_name"`
+	Class_owner  string     `json:"class_owner"`
+	Question     []Question `json:"question"`
+	Member       []member   `json:"member"`
+}
+
+type Question struct {
+	Question string `json:"question"`
+}
+
+type member struct {
+	Member string `json:"member"`
+}
